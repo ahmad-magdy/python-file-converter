ba9cc75f2eb8a78abf21c86746bea60432b05fbc
Ahmad Magdy <ahmad.magdy@talabat.com>
2025-08-19 14:15:21 +0300

    Final fix for Replit deployment configuration

--- start.sh
+++ start.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-# Install system packages (needed once per repl)
-if ! command -v tesseract &> /dev/null
-then
-  echo "Installing Tesseract..."
-  apt-get update -y
-  apt-get install -y tesseract-ocr
-fi
-
-# Install Python packages from requirements.txt
 pip install --upgrade -r requirements.txt
-
-# Start Flask
 python3 -m flask run
